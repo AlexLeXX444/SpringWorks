601b203dbb78b84e429f9265ce4cf5dda689c54d
Александр Гарейнов <adminuser@admin.local>
2025-07-10 00:38:32 +0500

    готова работа семь

---
+++ Lesson_7_SpringBoot_Security/application/src/main/java/ru/app/application/configuration/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package ru.app.application.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/private-data").hasRole("ADMIN")
+                        .requestMatchers("/public-data").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{noop}password")
+                        .roles("USER")
+                        .build(),
+                User.withUsername("admin")
+                        .password("{noop}password")
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+}
