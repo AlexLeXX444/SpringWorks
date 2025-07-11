308c7d02138075d0636f310eea68b66e40a0192c
Александр Гарейнов <adminuser@admin.local>
2025-07-11 20:12:30 +0500

    базовый проект девятой задачи

---
+++ Lesson_9_SpringCloud/application/reader-service/src/test/java/ru/app/readerservice/ReaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.app.readerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
